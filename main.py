c68b80e07331486796bd1b6084c6bcd0855d4280
scardona14 <cardanasean123@gmail.com>
2024-01-31 12:47:02 -0400

    PythonBasics

--- main.py
+++
@@ -1,8 +0,0 @@
-import random
-
-# Genarate a random list with randit between 1 and 1000
-# Use range to genarate 100 values
-randList = list(random.randint(1, 1001) for i in range(100))
-
-# Use modulus to find multiples of 9
-print(list(filter((lambda x: x % 9 == 0), randList)))
