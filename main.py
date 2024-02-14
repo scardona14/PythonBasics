20e96f69fe71b7cf6e43f635289af65a9247ee2f
scardona14 <cardanasean123@gmail.com>
2024-02-14 14:48:52 -0400

    PythonBasics-Receipts for Lovely Loveseats

---
+++ main.py
@@ -0,0 +1,32 @@
+# Product Descriptions
+lovely_loveseat_description = ("Lovely Loveseat. Tufted polyester blend on wood. 32 inches high x 40 inches wide x 30 inches deep. Red or white.")
+stylish_settee_description =("Stylish Settee. Faux leather on birch. 29.50 inches high x 54.75 inches wide x 28 inches deep. Black.")
+luxurious_lamp_description = ("Luxurious Lamp. Glass and iron. 36 inches tall. Brown with cream shade.")
+
+# Product Prices
+lovely_loveseat_price = 254.00
+stylish_settee_price = 180.50
+luxurious_lamp_price = 52.15
+
+# Sales Tax
+sales_tax = .088
+
+# Customer Totals
+customer_one_total = 0
+
+# Customer Itemizations
+customer_one_itemization = ""
+
+# Customer One purchase order
+customer_one_total += lovely_loveseat_price
+customer_one_itemization += lovely_loveseat_description
+customer_one_total += luxurious_lamp_price
+customer_one_itemization += " " + luxurious_lamp_description
+
+#Calculating one tax
+customer_one_tax = customer_one_total * sales_tax
+customer_one_total += sales_tax
+
+# Final Receipt
+print("Customer One Items: " + str(customer_one_itemization))
+print("Customer One Toal: " + str(customer_one_total))
