d967b5245c78b30af59472552258dd4f8c9d6d2a
scardona14 <cardanasean123@gmail.com>
2024-02-20 00:44:07 -0400

    Getting_to_know_you

---
+++ Get_to_know_you.py
@@ -0,0 +1,14 @@
+# Getting to know you
+
+My_name = input("What's your name?: ")
+My_favouite_food = input("What's your favourite food?: ")
+My_favourite_music = input("What's your favourite music?: ")
+My_location = input("Where do you live?: ")
+print("You are")
+print(My_name)
+print("You're probably hungry for")
+print(My_favouite_food)
+print("and you're definitely getting your groove on to")
+print(My_favourite_music)
+print("living in the amazing")
+print(My_location)
