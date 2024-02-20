f31a042c0d699de2580b8c6552408e31521e0b0a
scardona14 <cardanasean123@gmail.com>
2024-02-20 14:02:32 -0400

    random recipe

---
+++ random_recipe.py
@@ -0,0 +1,8 @@
+Food_name = input("Name a food > ")
+Plant_name = input("Name a type of plant > ")
+Cooking_method = input("Name a method of cooking > ")
+Burned_food = input("What word describes burned food? > ")
+DIY_item = input("Name a DIY item > ")
+
+print("MENU")
+print(Cooking_method, Food_name, "with", Burned_food, Plant_name, "on a bed of", DIY_item)
