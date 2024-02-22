f5e1750487f79270d94368e50c5eb005710f0ff9
scardona14 <cardanasean123@gmail.com>
2024-02-22 15:10:06 -0400

    fanke fan finder

---
+++ fake_fan_finder.py
@@ -0,0 +1,22 @@
+print("Fake Fan Finder")
+print("---------------")
+print()
+anime = input("What's your favorite Anime? ")
+if anime == "One Piece":
+  print("Oh really?!")
+  character = input("Name me any of the characters? ")
+  if character == "Nami":
+    print("You got that by pure chance.")
+    job = input("Okay then, what is her job on the ship? ")
+    if job == "Navigator":
+      print("Hmph!")
+      bounty = input("What was her first bounty then? ")
+      if bounty == "Ummm...":
+        print("See! FAKE One piece fan!")
+      else:
+        print("You are a true fan!")
+    else:
+      print("You are a true fan!")
+  else:
+    print("You are a true fan!")
+    
