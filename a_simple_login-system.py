657586e9da59e348adaab111d8a6f99fcfd19f0f
scardona14 <cardanasean123@gmail.com>
2024-02-22 00:56:24 -0400

    simple login program

---
+++ a_simple_login-system.py
@@ -0,0 +1,19 @@
+print("MY LOGIN SYSTEM")
+
+username = input("Username > ")
+password = input("Password> ")
+
+if username == "David" and password == "totallyNotBald":
+  print("Why hello there David, what a lovely accent you have, you could have charmed your way in here even without a password.")
+  print("Have a great day.")
+  print("Don't forget to wear a hat in the sun!")
+elif username == "Jack" and password == "Jack123":
+  print("Hey Jack, you are amazing!")
+  
+elif username == "Fernando" and password == "word":
+ print("Welcome Fernando!")
+  
+elif username == "Paola" and password == "Pao":
+ print("Hey there Paola!")
+else:
+ print("Go away, awayyy!")
