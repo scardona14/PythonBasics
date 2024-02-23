59080ef4d1c2cc359cc8526ea24e05cff4b28edb
scardona14 <cardanasean123@gmail.com>
2024-02-23 12:30:46 -0400

    Which Generation are you?

---
+++ .idea/generation_identifier.py
@@ -0,0 +1,21 @@
+print("Generation Identifier")
+print()
+year = int(input("Which year were you born? "))
+if year >= 1883 and year <= 1900:
+  print("Lost Generation")
+elif year >= 1901 and year <= 1927:
+  print("Greatest Generation")
+elif year >= 1928 and year <= 1945:
+  print("Silent Generation")
+elif year >= 1946 and year <= 1964:
+  print("Baby Boomers")
+elif year >= 1965 and year <= 1980:
+  print("Generation X")
+elif year >= 1981 and year <= 1996:
+  print("Millennials")
+elif year >= 1997 and year <= 2012:
+  print("Generation Z")
+elif year >= 2013 and year <= 2023:
+  print("Generation Alpha")
+else:
+  print("You are not born yet")
