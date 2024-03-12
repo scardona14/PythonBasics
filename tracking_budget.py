b06c1bb8feb617779b76a6a58357a6db9aeaba3d
scardona14 <cardanasean123@gmail.com>
2024-03-11 23:20:46 -0400

    Adding comment to program

--- tracking_budget.py
+++ tracking_budget.py
@@ -5,7 +5,7 @@ def print_remaining_budget(budget):
 
 print_remaining_budget(current_budget)
 
-# Write your code below: 
+# My code below: 
 def deduct_expense(budget, expense):
   return budget - expense
 shirt_expense = 9
